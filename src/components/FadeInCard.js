15f6ab8a028bb51e9a6e39fc5bdd14af76dc992f
Juan Molina Jiron <juanmolinajiron@gmail.com>
2021-08-31 16:42:57 -0400

    Added projects images for the underline project. Added styling and added extra things to fadein card component.

--- src/components/FadeInCard.js
+++ src/components/FadeInCard.js
@@ -1,18 +1,50 @@
 import React from "react"
-import { Card, Col } from "react-bootstrap"
+import { Card, Col, Badge, Row } from "react-bootstrap"
 
 function FadeInCard(props) {
-  const { image, title } = props
-  return (
-    <Col className='d-flex justify-content-center'>
-      <Card className='border-0 card-size'>
-        <Card.Img variant='top' src={image} className='card-img-size' />
-        <Card.Body>
-          <Card.Title className='text-center'>{title}</Card.Title>
-        </Card.Body>
-      </Card>
-    </Col>
-  )
+  const { image, title, projects, text, tools } = props
+  if (projects) {
+    return (
+      <Col className='d-flex justify-content-center'>
+        <Card className='card-projects-size'>
+          <Card.Img
+            variant='top'
+            src={image}
+            className='card-projects-img-size'
+          />
+          <Card.Body>
+            <Card.Title className='text-center'>{title}</Card.Title>
+            <Card.Text>{text}</Card.Text>
+            <Card.Subtitle className='text-center'>
+              Developed using:{" "}
+            </Card.Subtitle>
+            <Row sm={2} md={2} lg={3} xl={3} xxl={3}>
+              {tools.map(tool => {
+                return (
+                  <Col>
+                    <Badge key={tool} pill className='badge-bg-color'>
+                      {tool}
+                    </Badge>
+                  </Col>
+                )
+              })}
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  } else {
+    return (
+      <Col className='d-flex justify-content-center'>
+        <Card className='border-0 card-size'>
+          <Card.Img variant='top' src={image} className='card-img-size' />
+          <Card.Body>
+            <Card.Title className='text-center'>{title}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  }
 }
 
 export default FadeInCard
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,59 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import { Container, CardGroup, Row } from "react-bootstrap"
+import { useInView } from "react-intersection-observer"
+
+//components
+import FadeInCard from "./FadeInCard"
+
+//images
+import underlineCover from "../images/projects/underline.gif"
 
 function Projects() {
+  const { ref, inView, entry } = useInView()
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    if (inView) {
+      setIsVisible(inView)
+    }
+  }, [inView])
   return (
-    <div id="projects" className="custom-section">
-      <h1>Projects Section</h1>
-    </div>
+    <Container id='projects' fluid className='top-padding'>
+      <h1 className='text-center'>Projects</h1>
+      <CardGroup className='top-padding d-flex flex-row justify-content-center'>
+        <Row
+          sm={1}
+          md={1}
+          lg={3}
+          xl={3}
+          xxl={3}
+          ref={ref}
+          className={`fade-in ${isVisible ? "is-visible" : ""}`}
+        >
+          <FadeInCard
+            image={underlineCover}
+            title='The Underline'
+            projects={true}
+            text='Some example text here'
+            tools={["test1", "test2", "test3", "test4"]}
+          />
+          <FadeInCard
+            image={underlineCover}
+            title='The Underline'
+            projects={true}
+            text='Some example text here'
+            tools={["test1", "test2", "test3"]}
+          />
+          <FadeInCard
+            image={underlineCover}
+            title='The Underline'
+            projects={true}
+            text='Some example text here'
+            tools={["test1", "test2", "test3"]}
+          />
+        </Row>
+      </CardGroup>
+    </Container>
   )
 }
 
